043b1b1ff9ce088804d6b9a4a44bb6a210958a11
Stela Capsa <stela.capsa@gmail.com>
2020-02-19 19:03:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,17 @@
 class Student
   
   attr_accessor :name, :grade 
-  attr_reader :id, :grade
+  attr_reader :id
   
   
-  def initialize(name, garde, id = nil)
+  def initialize(name, garde = nil, id = nil)
     @name = name 
     @grade = grade
     @id = id 
+  end 
+  
+  def self.create_table
+    
   end 
 
   # Remember, you can access your database connection anywhere in this class
