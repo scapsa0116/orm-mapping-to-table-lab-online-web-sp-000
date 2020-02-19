9bf8e0b3acd344ff09f263fb77de266a1cb32b9a
Stela Capsa <stela.capsa@gmail.com>
2020-02-19 19:05:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,9 @@ class Student
   def self.create_table
     
   end 
-
+def save 
+  @@all << self 
+end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
